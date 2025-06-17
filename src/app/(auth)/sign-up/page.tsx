0a08815436ec8a9fc87345d464776d847842c57e
Sanchit Mishra <sanchitmishra9795@gmail.com>
2025-06-18 00:29:50 +0530

    Eliminate inconsistencies

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -103,7 +103,7 @@ export default function Page() {
             <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
                 <div className="text-center">
                     <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                        Join PureFeedback
+                        Join <div>TSB</div>
                     </h1>
                     <p className="mb-4">Sign up to start your anonymous adventure</p>
                 </div>
@@ -146,7 +146,7 @@ export default function Page() {
                                 <FormItem>
                                     <FormLabel>Email</FormLabel>
                                     <Input {...field} name="email" placeholder="username" />
-                                    <p className="text-muted text-gray-500 text-sm">
+                                    <p className="text-muted text-red-700 text-sm">
                                         We will send you a verification code
                                     </p>
                                     <FormMessage />
